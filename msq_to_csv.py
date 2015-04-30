8dbcc7dd0c27406444d161f7a52e976a78b315e5
phelantrinh <phelantrinh@users.noreply.github.com>
2015-04-30 10:29:57 +1000

    more general cases

--- msq_to_csv.py
+++ msq_to_csv.py
@@ -29,19 +29,60 @@ except: TypeError
     # Do nothing
 
 else:
-    for row in reader:       
-        if len(row) == 10:                               # Check if header row
+    cumTonnes = 'NaN'
+    parcelCount = 'NaN'
+    headerTonnes = 'NaN'
+    period = 'NaN'
+    fraction = 'NaN'
+    pushback = 'NaN'
+    parcels = 'NaN'
+    cumTonnes = 'NaN'
+    parcelCount = 'NaN'
+    headerFlag = 'NaN'
+    
+    for row in reader:
+        print row
+        try:
+            #Index 3 should be parcels (int) in a header line, rocktype (str) in parcel line 
+            #Try to convert to int...
+            parcels = int(row[3])
+        except ValueError:
+            #...raise ValueError exception if you try to convert string to int - probably a parcel line
+            headerFlag = 0
+        except IndexError:
+            #Header or parcel row should have at least 4 entries, if not, skip row.
+            continue
+        else:
+            headerFlag = 1
+                  
+        if headerFlag == 1:                               # Check if header row
+            headerTonnes = float(row[6])
             period = int(row[7])
             fraction = float(row[8])
             pushback = int(row[9])
-        elif len(row) == (6 + numGradeFields):           # Check if parcel row
-            # Multiply tonnage by fraction to get tonnage for period
-            tonnes = fraction * float(row[4])
+            parcels = int(row[3])
+            cumTonnes = 0.00                              # Reset cumulative tonnes for this block of parcels
+            parcelCount = 0                               # Reset parcel count for this block of parcels
+        elif len(row) == (6 + numGradeFields):            # Check if parcel row
+            # Read data for parcel row
+            rockType = row[3]
+            parcelTonnes = float(row[4])
+            elementQtys = row[5:(5+numGradeFields)]
+            process = row[-1]
+            print cumTonnes
+            print parcelTonnes
+            cumTonnes += parcelTonnes           
             
             # Concatnate header infomation with parcel information
-            conc_row = [period, fraction, pushback] + [row[3]] + [tonnes] \
-                            + row[5:6+(numGradeFields)]
-            writer.writerow(conc_row)
+            concRow = [period, fraction, pushback, rockType, parcelTonnes] + elementQtys + [process] 
+
+            writer.writerow(concRow)
+            parcelCount += 1
+
+            if parcelCount == parcels and cumTonnes < headerTonnes:
+                missingTonnes = headerTonnes - cumTonnes
+                unclass_row = [period, fraction, pushback, 'UNK', missingTonnes] + [0.0 for i in range(numGradeFields)] + ['-np-']
+                writer.writerow(unclass_row)
         else:
             print('WARNING - NOT HEADER OR PARCEL ROW: ')
             print(str(row))
